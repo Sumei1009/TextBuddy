c906460f766eafde66dae5584e0a56fec57adc42
SuSumei <susumei@178-105.priv23.nus.edu.sg>
2015-09-17 22:40:26 +0800

    Refatoring CE1 a little bit more

--- TextBuddyTestCases.java
+++ TextBuddyTestCases.java
@@ -5,29 +5,24 @@ import org.junit.Test;
 public class TextBuddyTestCases {
 
 	@Test
-	public void clearTest() {
+	public void executeCommandTest() {
 		TextBuddy.fileName = "a.txt";
-		assertEquals("all content deleted from a.txt", 
+		TextBuddy.readFileInLine("a.txt");
+		
+		assertEquals("all content deleted from a.txt",
 				TextBuddy.executeCommand("clear"));
+		assertEquals(0, TextBuddy.getLineCount());
+		
+		assertEquals("added to a.txt: \"I love CS!!!\"",
+				TextBuddy.executeCommand("add I love CS!!!"));
+		
+		String expected = TextBuddy.convertLineToString().trim();
+		assertEquals(expected, TextBuddy.executeCommand("display"));
+		
+		assertEquals("deleted from a.txt: \"I love CS!!!\"",
+				TextBuddy.executeCommand("delete 1"));
+		
 		assertEquals(0, TextBuddy.getLineCount());
 	}
 
-	@Test
-	public void addTest() {
-		TextBuddy.fileName = "a.txt";
-		assertEquals("added to a.txt: \"I love CS!!!\"", TextBuddy.executeCommand("add I love CS!!!"));
-	}
-	
-	@Test
-	public void displayTest() {
-		TextBuddy.fileName = "a.txt";
-		String expected = TextBuddy.convertLineToString().trim();
-		assertEquals(expected, TextBuddy.executeCommand("display"));
-	}
-	
-	@Test
-	public void deleteTest() {
-		TextBuddy.fileName = "a.txt";
-		assertEquals("deleted from a.txt: \"I love CS!!!\"", TextBuddy.executeCommand("delete 1"));
-	}
 }
